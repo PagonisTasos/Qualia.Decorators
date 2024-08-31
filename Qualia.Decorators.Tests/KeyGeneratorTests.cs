08ec7560e7674713d91498743701f41947b996c9
Tassos Pagonis <tpg@entersoft.gr>
2024-08-31 18:00:25 +0300

    add tests

---
+++ Qualia.Decorators.Tests/KeyGeneratorTests.cs
@@ -0,0 +1,62 @@
+﻿using Qualia.Decorators.Utils;
+
+namespace Qualia.Decorators.Tests
+{
+    [TestFixture]
+    public class KeyGeneratorTests
+    {
+        [Test]
+        public void CreateKey_GeneratesConsistentKey_ForSameMethodAndArgs()
+        {
+            // Arrange
+            var methodInfo = typeof(SampleClass).GetMethod(nameof(SampleClass.SampleMethod));
+            var args = new object[] { 1, 2 };
+
+            // Act
+            var key1 = KeyGenerator.CreateKey(methodInfo, args);
+            var key2 = KeyGenerator.CreateKey(methodInfo, args);
+
+            // Assert
+            Assert.That(key2, Is.EqualTo(key1));
+        }
+
+        [Test]
+        public void CreateKey_GeneratesDifferentKeys_ForDifferentMethods()
+        {
+            // Arrange
+            var methodInfo1 = typeof(SampleClass).GetMethod(nameof(SampleClass.SampleMethod));
+            var methodInfo2 = typeof(SampleClass).GetMethod(nameof(SampleClass.AnotherMethod));
+            var args = new object[] { 1, 2 };
+
+            // Act
+            var key1 = KeyGenerator.CreateKey(methodInfo1, args);
+            var key2 = KeyGenerator.CreateKey(methodInfo2, args);
+
+            // Assert
+            Assert.That(key2, Is.Not.EqualTo(key1));
+        }
+
+        [Test]
+        public void CreateKey_GeneratesDifferentKeys_ForDifferentArgs()
+        {
+            // Arrange
+            var methodInfo = typeof(SampleClass).GetMethod(nameof(SampleClass.SampleMethod));
+            var args1 = new object[] { 1, 2 };
+            var args2 = new object[] { 2, 3 };
+
+            // Act
+            var key1 = KeyGenerator.CreateKey(methodInfo, args1);
+            var key2 = KeyGenerator.CreateKey(methodInfo, args2);
+
+            // Assert
+            Assert.That(key2, Is.Not.EqualTo(key1));
+        }
+
+        private class SampleClass
+        {
+            public int SampleMethod(int a, int b) => a + b;
+
+            public int AnotherMethod(int a, int b) => a * b;
+        }
+    }
+}
---
+++ Qualia.Decorators.Tests/MemoizeTests.cs
@@ -0,0 +1,80 @@
+﻿using Moq;
+using Microsoft.Extensions.Logging;
+using Qualia.Decorators.Framework;
+
+
+namespace Qualia.Decorators.Tests
+{
+
+    [TestFixture]
+    public class MemoizeTests
+    {
+        private Memoize _memoize;
+        private Mock<ILogger<Memoize>> _mockLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockLogger = new Mock<ILogger<Memoize>>();
+            _memoize = new Memoize(_mockLogger.Object);
+        }
+
+        [Test]
+        public void Invoke_CachesResult_AfterFirstCall()
+        {
+            // Arrange
+            var methodInfo = typeof(SampleClass).GetMethod(nameof(SampleClass.SampleMethod));
+            var context = new DecoratorContext<SampleClass>(null, new SampleClass(), methodInfo, new object[] { 1, 2 });
+
+            // Act
+            var result1 = _memoize.Invoke(context);
+            var result2 = _memoize.Invoke(context);
+
+            // Assert
+            Assert.That(result2, Is.EqualTo(result1));
+        }
+
+        [Test]
+        public void Invoke_DoesNotCacheDifferentMethod()
+        {
+            // Arrange
+            var methodInfo1 = typeof(SampleClass).GetMethod(nameof(SampleClass.SampleMethod));
+            var methodInfo2 = typeof(SampleClass).GetMethod(nameof(SampleClass.AnotherMethod));
+
+            var context1 = new DecoratorContext<SampleClass>(null, new SampleClass(), methodInfo1, new object[] { 1, 2 });
+            var context2 = new DecoratorContext<SampleClass>(null, new SampleClass(), methodInfo2, new object[] { 1, 2 });
+
+            // Act
+            var result1 = _memoize.Invoke(context1);
+            var result2 = _memoize.Invoke(context2);
+
+            // Assert
+            Assert.That(result2, Is.Not.EqualTo(result1));
+        }
+
+        [Test]
+        public void Invoke_CachesWithDifferentArguments()
+        {
+            // Arrange
+            var methodInfo = typeof(SampleClass).GetMethod(nameof(SampleClass.SampleMethod));
+
+            var context1 = new DecoratorContext<SampleClass>(null, new SampleClass(), methodInfo, new object[] { 1, 2 });
+            var context2 = new DecoratorContext<SampleClass>(null, new SampleClass(), methodInfo, new object[] { 2, 3 });
+
+            // Act
+            var result1 = _memoize.Invoke(context1);
+            var result2 = _memoize.Invoke(context2);
+
+            // Assert
+            Assert.That(result2, Is.Not.EqualTo(result1));
+        }
+
+        private class SampleClass
+        {
+            public int SampleMethod(int a, int b) => a + b;
+
+            public int AnotherMethod(int a, int b) => a * b;
+        }
+    }
+
+}
