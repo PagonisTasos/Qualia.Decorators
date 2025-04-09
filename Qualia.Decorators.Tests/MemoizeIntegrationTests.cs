c516df4ee2f1dd4fe85c8e9dcec25cf786ce56d2
Tasos Pagonis <tpg@entersoft.gr>
2025-04-09 20:09:24 +0300

    update integration tests

--- Qualia.Decorators.Tests/MemoizeIntegrationTests.cs
+++ Qualia.Decorators.Tests/MemoizeIntegrationTests.cs
@@ -9,23 +9,16 @@ namespace Qualia.Decorators.Tests
     public class MemoizeIntegrationTests
     {
         [Test]
-        public void ImplementationFactory_w_ConcreteClassReturnType_Bar_Method_Should_Be_Memoized()
+        public void ConcreteImplementationType_Should_Be_Memoized()
         {
             // Arrange
-
             IServiceCollection services = new ServiceCollection();
 
-            // Create a mock of the Foo class implementing IFoo interface
-            var _fooMock = new Mock<Foo>();
             var loggerMock = new Mock<ILogger<Memoize>>();
-
-            // Set up the Bar method to count invocations
-            _fooMock.Setup(f => f.Bar()).Verifiable();
-
-            // Register the mock in the service collection
             services.AddScoped(_ => loggerMock.Object);
-
-            services.AddSingleton<IFoo, Foo>(_ => _fooMock.Object);
+            services.AddTransient<Memoize>();
+            services.AddSingleton<IFoo, Foo>();
+            services.AddSingleton<IFoo2, Foo2>();
 
             // Apply the decorators
             services.UseDecorators();
@@ -39,27 +32,21 @@ namespace Qualia.Decorators.Tests
             foo.Bar(); // Second call - should hit the cache
 
             // Assert
-            _fooMock.Verify(f => f.Bar(), Times.Once, "Bar method should only be called once due to memoization.");
+            Assert.That(foo.Counter, Is.EqualTo(1));
         }
 
         [Test]
-        public void ImplementationInstance_w_AbstractClassType_Bar_Method_Should_Be_Memoized()
+        public void ConcreteImplementationFactory_Should_Be_Memoized()
         {
             // Arrange
-
             IServiceCollection services = new ServiceCollection();
 
-            // Create a mock of the Foo class implementing IFoo interface
-            var _fooMock = new Mock<Foo>();
             var loggerMock = new Mock<ILogger<Memoize>>();
-
-            // Set up the Bar method to count invocations
-            _fooMock.Setup(f => f.Bar()).Verifiable();
-
-            // Register the mock in the service collection
             services.AddScoped(_ => loggerMock.Object);
-
-            services.AddSingleton<IFoo>(_fooMock.Object);
+            services.AddTransient<Memoize>();
+            var foo2 = new Foo2();
+            services.AddSingleton<IFoo, Foo>(_ => new Foo(foo2));
+            services.AddSingleton<IFoo2, Foo2>(_ => foo2);
 
             // Apply the decorators
             services.UseDecorators();
@@ -73,16 +60,93 @@ namespace Qualia.Decorators.Tests
             foo.Bar(); // Second call - should hit the cache
 
             // Assert
-            _fooMock.Verify(f => f.Bar(), Times.Once, "Bar method should only be called once due to memoization.");
+            Assert.That(foo.Counter, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AbstractImplementationFactory_Should_Be_Memoized()
+        {
+            // Arrange
+            IServiceCollection services = new ServiceCollection();
+
+            var loggerMock = new Mock<ILogger<Memoize>>();
+            services.AddScoped(_ => loggerMock.Object);
+            services.AddTransient<Memoize>();
+            var foo2 = new Foo2();
+            services.AddSingleton<IFoo>(_ => new Foo(foo2));
+            services.AddSingleton<IFoo2>(_ => foo2);
+
+            // Apply the decorators
+            services.UseDecorators();
+
+            // Build the service provider
+            var _serviceProvider = services.BuildServiceProvider();
+            var foo = _serviceProvider.GetRequiredService<IFoo>();
+
+            // Act
+            foo.Bar(); // First call
+            foo.Bar(); // Second call - should hit the cache
+
+            // Assert
+            Assert.That(foo.Counter, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AbstractImplementationInstance_Should_Be_Memoized()
+        {
+            // Arrange
+            IServiceCollection services = new ServiceCollection();
+
+            var loggerMock = new Mock<ILogger<Memoize>>();
+            services.AddScoped(_ => loggerMock.Object);
+            services.AddTransient<Memoize>();
+            var foo2 = new Foo2();
+            services.AddSingleton<IFoo>(new Foo(foo2));
+            services.AddSingleton<IFoo2>(foo2);
+
+            // Apply the decorators
+            services.UseDecorators();
+
+            // Build the service provider
+            var _serviceProvider = services.BuildServiceProvider();
+            var foo = _serviceProvider.GetRequiredService<IFoo>();
+
+            // Act
+            foo.Bar(); // First call
+            foo.Bar(); // Second call - should hit the cache
+
+            // Assert
+            Assert.That(foo.Counter, Is.EqualTo(1));
         }
 
         public interface IFoo
         {
             void Bar();
+            int Counter { get; }
         }
 
         [Memoize]
         public class Foo : IFoo
+        {
+            public Foo(IFoo2 foo)
+            {
+                    
+            }
+            public virtual void Bar()
+            { 
+                Counter++;
+            }
+
+            public int Counter { get; private set; }
+        }
+
+        public interface IFoo2
+        {
+            void Bar();
+        }
+
+        [Memoize]
+        public class Foo2 : IFoo2
         {
             public virtual void Bar()
             { }
